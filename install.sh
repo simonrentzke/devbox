7da8b316880ed0d4ae73fc2dac3c668c4302bc58
Simon Rentzke <simon@rentzke.com>
2019-10-07 11:43:50 +1000

    Extra installation

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
 
 sudo apt-get install -y zsh
+sudo install -y ghi
+sudo snap install hub --classic
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)  --skip-chsh"
 rm $HOME/.zshrc
