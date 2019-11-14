aa30fb2a4ee037022ffa7b31089afce6a433a73c
Simon Rentzke <simon@rentzke.com>
2019-11-14 10:57:06 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 sudo apt-get update
 
-sudo apt-get install -y zsh
-sudo apt-get install -y ghi
 sudo snap install hub --classic
 
+cd ~/exceed && gem install ghi
+
+sudo apt-get install -y zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)  --skip-chsh"
+
 rm $HOME/.zshrc
+
 cp -RTs $PWD/home $HOME
+
 source $HOME/.zshrc
