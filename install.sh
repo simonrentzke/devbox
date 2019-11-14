cf14eab397e2c01700ccfbd3444c932cc1adb669
Simon Rentzke <simon@rentzke.com>
2019-11-14 13:21:14 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ sudo apt-get install -y zsh
 
 zsh
 
+chsh -s /bin/zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)  --skip-chsh"
 
 rm $HOME/.zshrc
