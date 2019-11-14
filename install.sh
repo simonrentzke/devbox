17157a6f41a45b0c31fbd2515a62e20d84f8bae6
Simon Rentzke <simon@rentzke.com>
2019-11-14 13:19:44 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ cd $HOME/exceed && gem install ghi
 
 sudo apt-get install -y zsh
 
+zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)  --skip-chsh"
 
 rm $HOME/.zshrc
